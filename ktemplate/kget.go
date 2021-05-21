4c1f93d256c8b255d9d8fadd00805a93a2866e78
Tarun Khandelwal <tarun@flanksource.com>
2021-05-21 15:22:03 +0530

    Revert "feat: update functions struct to use kommons.client and kget to searc…"

--- ktemplate/kget.go
+++ ktemplate/kget.go
@@ -1,11 +1,14 @@
 package ktemplate
 
 import (
+	"context"
 	"encoding/json"
 	"strings"
 
 	"github.com/flanksource/commons/logger"
 	"github.com/tidwall/gjson"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func (f *Functions) KGet(path, jsonpath string) string {
@@ -19,44 +22,41 @@ func (f *Functions) KGet(path, jsonpath string) string {
 	namespace := parts[1]
 	name := parts[2]
 
-	if kind == "configmap" || kind == "cm" || kind == "ConfigMap" {
-		configMapData := f.clientset.GetConfigMap(namespace, name)
-		if configMapData == nil {
-			logger.Errorf("failed to read configmap name %s namespace %s: %v", name, namespace)
+	if kind == "configmap" || kind == "cm" {
+		cm, err := f.clientset.CoreV1().ConfigMaps(namespace).Get(context.Background(), name, metav1.GetOptions{})
+		if err != nil {
+			if !errors.IsNotFound(err) {
+				logger.Errorf("failed to read configmap name %s namespace %s: %v", name, namespace, err)
+			}
 			return ""
 		}
 
-		data := *configMapData
-		return data[jsonpath]
-	} else if kind == "secret" || kind == "Secret" {
-		secretData := f.clientset.GetSecret(namespace, name)
-		if secretData == nil {
-			logger.Errorf("failed to read secret name %s namespace %s: %v", name, namespace)
-			return ""
-		}
-		data := *secretData
-		return string(data[jsonpath])
-	} else if kind == "service" || kind == "svc" {
-		svc, err := f.clientset.GetByKind("Service", namespace, name)
-		if svc == nil || err != nil {
-			logger.Errorf("failed to read service name %s namespace %s: %v", name, namespace, err)
-			return ""
-		}
-
-		encodedJSON, err := json.Marshal(svc.Object)
+		encodedJSON, err := json.Marshal(cm)
 		if err != nil {
 			logger.Errorf("failed to encode json name %s namespace %s: %v", name, namespace, err)
 			return ""
 		}
 		value := gjson.Get(string(encodedJSON), jsonpath)
 		return value.String()
-	} else if kind != "" {
-		object, err := f.clientset.GetByKind(kind, namespace, name)
-		if object == nil || err != nil {
-			logger.Errorf("failed to read %s name %s namespace %s: %v", kind, name, namespace, err)
+	} else if kind == "secret" {
+		secret, err := f.clientset.CoreV1().Secrets(namespace).Get(context.Background(), name, metav1.GetOptions{})
+		if err != nil {
+			if !errors.IsNotFound(err) {
+				logger.Errorf("failed to read secret name %s namespace %s: %v", name, namespace, err)
+			}
 			return ""
 		}
-		encodedJSON, err := json.Marshal(object.Object)
+		return string(secret.Data[jsonpath])
+	} else if kind == "service" || kind == "svc" {
+		svc, err := f.clientset.CoreV1().Services(namespace).Get(context.Background(), name, metav1.GetOptions{})
+		if err != nil {
+			if !errors.IsNotFound(err) {
+				logger.Errorf("failed to read service name %s namespace %s: %v", name, namespace, err)
+			}
+			return ""
+		}
+
+		encodedJSON, err := json.Marshal(svc)
 		if err != nil {
 			logger.Errorf("failed to encode json name %s namespace %s: %v", name, namespace, err)
 			return ""
